c900da76eff25cdacb17583277ae24c62d7519ba
Juan Manzanero <juan.manzanero@upm.es>
2022-08-30 22:40:50 +0200

    [JMT] Fixed the commit hash corresponding to which Lion version should be used to run each version

--- cmake/third-party/lion.cmake
+++ cmake/third-party/lion.cmake
@@ -1,8 +1,11 @@
 if (${BUILD_LION})
+
+set(lion_version 0a45a6a99e4d393720a5d0678e414d639cb1419d)
+
 include(ExternalProject)
 ExternalProject_Add(lion
     GIT_REPOSITORY https://github.com/juanmanzanero/lion-cpp.git
-    GIT_TAG main
+    GIT_TAG ${lion_version}
     CMAKE_ARGS -DENABLE_TEST=OFF -DCMAKE_INSTALL_PREFIX=${THIRD_PARTY_DIR}/thirdparty -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
     PREFIX ${THIRD_PARTY_DIR}/lion
     SOURCE_DIR "${THIRD_PARTY_DIR}/lion/source"
